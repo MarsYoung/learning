b81a6de567e0b6da429e6f0c1888843de9a71a3f
MarsYoung <bigmazhiyu@gmail.com>
2015-03-23 13:52:56 +0800

    initial project version

---
+++ learning-orgin/src/main/java/com/marsyoung/origin/App.java
@@ -0,0 +1,13 @@
+package com.marsyoung.origin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
