97d1cc4460f878a790145fbb86ef4b823f90cea5
MarsYoung <bigmazhiyu@gmail.com>
2015-04-07 09:45:14 +0800

    自定义java键值对集合

--- learning-orgin/src/test/java/com/marsyoung/test/JavaCollection.java
+++ learning-orgin/src/test/java/com/marsyoung/test/JavaCollection.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedSet;
+import java.util.Stack;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.Vector;
@@ -25,10 +27,48 @@ public class JavaCollection {
 	List<?> list = new Vector<>();//Vector是线程同步的
 	List<?> list1 = new ArrayList<>();//性能上要比Vector好一些,但不同步
 	List<?> list2 = new LinkedList<>();//LinkedList不同于前面两种List，它不是基于数组的，所以不受数组性能的限制。
-	
+	List<?> list3 =new Stack<>();
 	Map<?,?> map =new HashMap<Integer,String>();
 	Map<?,?> map1 =new TreeMap<Integer,String>();//SortedMap
+	Map<?,?> map2 =new LinkedHashMap<Integer,String>();
 	
 	Hashtable<Integer, String> hb=new Hashtable<>();
+	
+	
+	//test array 
+	int capacity=16;
+	String[] x=new String[capacity];
+	
+	class KeyValue<K,V> implements Map.Entry<K, V>{
+		final K key;
+        V value;
+        KeyValue<K,V> next;
+        final int hash;
+        
+        KeyValue(int h, K k, V v, KeyValue<K,V> n) {
+            value = v;
+            next = n;
+            key = k;
+            hash = h;
+        }
+		@Override
+		public K getKey() {
+			return key;
+		}
+		@Override
+		public V getValue() {
+			return value;
+		}
+		@Override
+		public V setValue(V newValue) {
+			V oldValue=value;
+			value= newValue;
+			return oldValue;
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	KeyValue[] kvArray=new KeyValue[capacity];
+	
 	
 }
