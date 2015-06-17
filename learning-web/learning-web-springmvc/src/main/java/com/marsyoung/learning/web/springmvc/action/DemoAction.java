3baad50f68c854361265939024cbc210dea13283
MarsYoung <bigmazhiyu@gmail.com>
2015-06-17 15:40:29 +0800

    web升级到3.0配置

--- learning-web/learning-web-springmvc/src/main/java/com/marsyoung/learning/web/springmvc/action/DemoAction.java
+++ learning-web/learning-web-springmvc/src/main/java/com/marsyoung/learning/web/springmvc/action/DemoAction.java
@@ -3,11 +3,13 @@ package com.marsyoung.learning.web.springmvc.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class DemoAction extends BaseAction {
 
 	@RequestMapping(value = "/selectUser", method = RequestMethod.GET)
