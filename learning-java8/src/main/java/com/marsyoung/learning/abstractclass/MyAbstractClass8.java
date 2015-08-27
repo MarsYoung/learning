ccf87d65e9f1f9e29917cf8b17bc8d15a8eb7a26
MarsYoung <bigmazhiyu@gmail.com>
2015-08-27 23:11:38 +0800

    几个java8测试类

---
+++ learning-java8/src/main/java/com/marsyoung/learning/abstractclass/MyAbstractClass8.java
@@ -0,0 +1,8 @@
+package com.marsyoung.learning.abstractclass;
+
+public  abstract class MyAbstractClass8 implements MyInterface8{
+	@Override
+	public void doSomething() {
+		System.out.println("dosomething");
+	}
+}
---
+++ learning-java8/src/main/java/com/marsyoung/learning/abstractclass/Test.java
@@ -0,0 +1,11 @@
+package com.marsyoung.learning.abstractclass;
+
+public class Test {
+
+	public static void main(String[] args) {
+		new MyClass8().doSomething();
+		new MyClass82().doSomething();
+
+	}
+
+}
