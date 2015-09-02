bed5229d48c11e7f75f931a532c366fcdeded68d
MarsYoung <bigmazhiyu@gmail.com>
2015-09-02 11:38:16 +0800

    基本功能调试

--- learning-orgin/src/main/java/com/marsyoung/file/FindFileApp.java
+++ learning-orgin/src/main/java/com/marsyoung/file/FindFileApp.java
@@ -1,70 +1,95 @@
 package com.marsyoung.file;
 
-
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
- 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.HashSet;
+
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPasswordField;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
- 
-public class FindFileApp extends JFrame {
+
+public class FindFileApp extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 8119185320549329260L;
-	
+
 	private JLabel fileDirectoryLabel;
 	private JLabel keywordLabel;
 	private JFileChooser fileDirectoryChooser;
-	private JTextField fileTextField;
+	private JTextField keywordText;
+	private JTextField fileDirectoryText;
 	private JLabel consoleLabel;
 	private JTextArea consoleArea;
-	
-	
-	private JButton btLog;
-    private JTextField tfUser;
-    private JPasswordField tfPwd;
-    private JCheckBox pwdKeep;
-    private JComboBox adminType;
- 
-    public FindFileApp() {
-        super("海底捞针");
-        super.setSize(380, 292);
-        super.setVisible(true);
-        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        centered(this);
-        btLog = new JButton("登     录");
-        btLog.setBounds(new Rectangle(93, 220, 180, 30));//参数分别是坐标x，y，宽，高
-        this.setLayout(null);//设置布局管理器为空
-        this.add(btLog);
-        tfUser = new JTextField();
-        tfUser.setBounds(new Rectangle(73, 115, 220, 25));
-        this.add(tfUser);
-        tfPwd = new JPasswordField();
-        tfPwd.setBounds(new Rectangle(73, 150, 220, 25));
-        this.add(tfPwd);
-        pwdKeep = new JCheckBox("记住密码");
-        pwdKeep.setBounds(new Rectangle(68, 185, 110, 25));
-        this.add(pwdKeep);
-        adminType = new JComboBox(new String[] { "普通职员", "管理员", "高级管理员" });
-        adminType.setBounds(new Rectangle(183, 185, 100, 25));
-        this.add(adminType);
- 
-    }
-    //布局居中方法
-    public void centered(Container container) {
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension screenSize = toolkit.getScreenSize();
-        int w = container.getWidth();
-        int h = container.getHeight();
-        container.setBounds((screenSize.width - w) / 2,
-                (screenSize.height - h) / 2, w, h);
-    }
+	private JButton searchButton;
+
+	public FindFileApp() {
+		super("海底捞针");
+		super.setSize(600, 400);
+		super.setVisible(true);
+		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		centered(this);
+		this.setLayout(null);// 设置布局管理器为空
+		fileDirectoryLabel = new JLabel("搜索路径：");
+		fileDirectoryLabel.setBounds(new Rectangle(0, 0, 180, 30));// 参数分别是坐标x，y，宽，高
+		this.add(fileDirectoryLabel);
+		keywordLabel = new JLabel("关键词：");
+		keywordLabel.setBounds(new Rectangle(0, 50, 180, 30));// 参数分别是坐标x，y，宽，高
+		this.add(keywordLabel);
+		consoleLabel = new JLabel("搜索结果：");
+		consoleLabel.setBounds(new Rectangle(0, 80, 180, 30));// 参数分别是坐标x，y，宽，高
+		this.add(consoleLabel);
+		fileDirectoryText = new JTextField();
+		fileDirectoryText.setBounds(new Rectangle(200, 0, 180, 30));
+		this.add(fileDirectoryText);
+		keywordText = new JTextField();
+		keywordText.setBounds(new Rectangle(200, 50, 180, 30));
+		this.add(keywordText);
+		consoleArea = new JTextArea();
+		consoleArea.setBounds(0, 400, 400, 400);
+		this.add(consoleArea);
+		searchButton = new JButton("搜索");
+		searchButton.setBounds(300, 300, 180, 30);
+		searchButton.addActionListener(this);
+		this.add(searchButton);
+	}
+
+	// 布局居中方法
+	public void centered(Container container) {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = toolkit.getScreenSize();
+		int w = container.getWidth();
+		int h = container.getHeight();
+		container.setBounds((screenSize.width - w) / 2, (screenSize.height - h) / 2, w, h);
+	}
+
+	public static void main(String[] args) {
+		new FindFileApp();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (fileDirectoryText.getText().equals("")) {
+			chooseFileDirectory();
+		} else if (keywordText.getText().equals("")) {
+		} else {
+			new FindFiles().find(new HashSet<File>(), fileDirectoryText.getText(), keywordText.getText());
+		}
+
+	}
+
+	public void chooseFileDirectory() {
+		fileDirectoryChooser = new JFileChooser();
+		fileDirectoryChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int result = fileDirectoryChooser.showDialog(new JLabel(), "选择");
+		if (result == JFileChooser.APPROVE_OPTION) {
+			fileDirectoryText.setText(fileDirectoryChooser.getSelectedFile().getAbsolutePath());
+		}
+	}
 }
