cb0c093303af47fe0a7e4acd3d6090a4192acc02
mazhiyu <444177096@qq.com>
2016-09-21 16:40:37 +0800

    add

---
+++ learning-transaction/src/main/java/com/marsyoung/transation/MyBean.java
@@ -0,0 +1,36 @@
+package com.marsyoung.transation;
+
+/**
+ * Created by mazhiyu on 16/7/15.
+ */
+public class MyBean {
+
+    int id;
+    String name;
+    int status;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
