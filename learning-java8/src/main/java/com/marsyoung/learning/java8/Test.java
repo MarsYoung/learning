42055d3509655ca97b2d0cbbff567d5677b6eddf
mazhiyu <444177096@qq.com>
2017-02-06 16:48:51 +0800

    add

--- learning-java8/src/main/java/com/marsyoung/learning/java8/Test.java
+++ learning-java8/src/main/java/com/marsyoung/learning/java8/Test.java
@@ -10,21 +10,20 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) {
-
-        List<Integer> integerList= Arrays.asList(90,99,100,490,500,990,1000,1500,1990,2000,2990,3000,5880,5990,8800,9800,9990);
-        for(int x:integerList){
-            String s="";
-            if(x<100){
-                s=0+"."+x;
-            }else{
-                s=x+"";
-                int l=s.length();
-                String tmp=s.substring(0,l-2)+"."+s.substring(l-2,l);
-                s=tmp;
-            }
-            check(x,s);
-        }
-
+//
+//        List<Integer> integerList= Arrays.asList(90,99,100,490,500,990,1000,1500,1990,2000,2990,3000,5880,5990,8800,9800,9990);
+//        for(int x:integerList){
+//            String s="";
+//            if(x<100){
+//                s=0+"."+x;
+//            }else{
+//                s=x+"";
+//                int l=s.length();
+//                String tmp=s.substring(0,l-2)+"."+s.substring(l-2,l);
+//                s=tmp;
+//            }
+//            check(x,s);
+//        }
 
 
     }
