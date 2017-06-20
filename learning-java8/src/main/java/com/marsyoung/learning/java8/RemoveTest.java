43ae894012475629bd6e0b7c29e563d15702e1b5
mazhiyu <444177096@qq.com>
2017-06-20 16:15:57 +0800

    rm unuse file

---
+++ learning-java8/src/main/java/com/marsyoung/learning/java8/RemoveTest.java
@@ -0,0 +1,29 @@
+package com.marsyoung.learning.java8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+//import java.util.stream.Collectors;
+
+/**
+ * Created by mazhiyu on 2017/6/20.
+ */
+public class RemoveTest {
+
+    public static void main(String[] args) {
+//        Set<Key> notCached = Arrays.asList(ids).parallelStream().collect(Collectors.toSet());
+//        Set<String> keys = notCached.parallelStream().map(one -> buildCachePrefix(one)).collect(Collectors.toSet());
+//        List<Entity> resultFromCache = redisCache.mget(keys);
+//        if (resultFromCache != null && resultFromCache.size() != 0) {
+//            resultFromCache.parallelStream().forEach(n -> {
+//                if (n != null) {
+//                    cacheResult.add(n);
+//                    notCached.remove(n.getCacheKey());
+//                }
+//            });
+//        }
+//        return notCached;
+
+    }
+}
