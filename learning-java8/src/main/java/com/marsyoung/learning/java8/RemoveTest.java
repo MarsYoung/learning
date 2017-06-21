eb6479cef2149934ff9109266bb84ccae6c8436c
mazhiyu <444177096@qq.com>
2017-06-21 11:13:19 +0800

    parallelStream和foreach一起用的不安全操作。

--- learning-java8/src/main/java/com/marsyoung/learning/java8/RemoveTest.java
+++ learning-java8/src/main/java/com/marsyoung/learning/java8/RemoveTest.java
@@ -4,26 +4,47 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
-//import java.util.stream.Collectors;
+import java.util.stream.Collectors;
 
 /**
  * Created by mazhiyu on 2017/6/20.
  */
+//parallelStream和foreach一起用会异步。
 public class RemoveTest {
 
+    public void unsafe(){
+        Set<String> notCached = Arrays.asList(1,2,3,4,5,6,7,8,9,10).parallelStream().map(n->n+"").collect(Collectors.toSet());
+        List<Integer> resultFromCache = Arrays.asList(1,2,3,4,5);
+        if (resultFromCache != null && resultFromCache.size() != 0) {
+            resultFromCache.parallelStream().forEach(n -> {
+                if (n != null) {
+                    notCached.remove(n+"");
+                }
+            });
+        }
+        System.out.println("unsafe:"+notCached.size());
+    }
+
+    public void safe(){
+        Set<String> notCached = Arrays.asList(1,2,3,4,5,6,7,8,9,10).parallelStream().map(n->n+"").collect(Collectors.toSet());
+        List<Integer> resultFromCache = Arrays.asList(1,2,3,4,5);
+        if (resultFromCache != null && resultFromCache.size() != 0) {
+            resultFromCache.stream().forEach(n -> {
+                if (n != null) {
+                    notCached.remove(n+"");
+                }
+            });
+        }
+        System.out.println("safe:"+notCached.size());
+    }
+
     public static void main(String[] args) {
-//        Set<Key> notCached = Arrays.asList(ids).parallelStream().collect(Collectors.toSet());
-//        Set<String> keys = notCached.parallelStream().map(one -> buildCachePrefix(one)).collect(Collectors.toSet());
-//        List<Entity> resultFromCache = redisCache.mget(keys);
-//        if (resultFromCache != null && resultFromCache.size() != 0) {
-//            resultFromCache.parallelStream().forEach(n -> {
-//                if (n != null) {
-//                    cacheResult.add(n);
-//                    notCached.remove(n.getCacheKey());
-//                }
-//            });
-//        }
-//        return notCached;
+        RemoveTest test=new RemoveTest();
+        for(int i=0;i<100;i++){
+            test.unsafe();
+            test.safe();
+        }
+
 
     }
 }
